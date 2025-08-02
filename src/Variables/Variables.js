6d52e083d233a7ce8bcd0bc8eb6135005285908a
Alex Dolid <dolid.sasha@gmail.com>
2025-08-02 20:24:43 +0300

    Refactor Variables class to support zod schemas
    Replaces schema parsing with a more flexible approach using zod schemas. Adds static parseShape method and updates constructor to handle both schema and zod options. Internal shape is now private and accessed via a getter.

--- src/Variables/Variables.js
+++ src/Variables/Variables.js
@@ -1,12 +1,24 @@
-const { VariablesSchemaSchema } = require('./schemas.js');
+const { VariablesZodSchemas } = require('./schemas.js');
 
 class Variables {
-  constructor(schema) {
-    this.schema = VariablesSchemaSchema.parse(schema);
+  static parseShape(shape, { schema, z } = {}) {
+    if (schema) return schema.parse(shape);
+    if (z) return VariablesZodSchemas.buildShape(z).parse(shape);
+    return shape;
+  }
+
+  #shape = null;
+
+  constructor(shape, { z } = {}) {
+    this.#shape = Variables.parseShape(shape, { z });
+  }
+
+  get shape() {
+    return this.#shape;
   }
 
   get(req) {
-    return Object.fromEntries(Object.entries(this.schema).map(([name, fn]) => [name, fn(req)]));
+    return Object.fromEntries(Object.entries(this.#shape).map(([name, fn]) => [name, fn(req)]));
   }
 }
 
